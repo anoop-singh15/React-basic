5db304fcb29f859aef1613c9bc86d878aba3b43c
cozytuba <cozytuba1533@gmail.com>
2024-07-18 10:26:57 +0530

    Understanding basic concept of jsx and its fragments

---
+++ 3-jsx-basic/src/App.js
@@ -0,0 +1,51 @@
+
+import './App.css';
+let a="world"
+function App() {
+  return (
+    // <>
+    
+      // <h1>sadsads</h1>
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <p>
+      //       Edit <code>src/App.js</code> and save to reload.
+      //     </p>
+      //     <a
+      //       className="App-link"
+      //       href="https://reactjs.org"
+      //       target="_blank"
+      //       rel="noopener noreferrer"
+      //     >
+      //     XYZ
+      //     </a>
+      //   </header>
+      // </div>
+
+
+    // </>
+
+    <>
+
+      <nav>
+        <li className="list">home</li>
+        <li className="list">about</li>
+        <li className="list">contact us</li>
+        <li className="list">login</li>
+      </nav>
+
+        <h1>
+          Hello  {a}
+        </h1>
+      <div className="blank">
+       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit voluptatem quibusdam aut? Ea, officiis enim aut et quod nostrum reprehenderit sunt, nisi impedit temporibus aspernatur, quam dicta libero quibusdam voluptate distinctio nulla! Totam placeat voluptatum, veritatis fugit quae ex in eum dolores vel autem, illo doloremque iure neque, tenetur asperiores.
+      </div>
+
+
+    </>
+
+  );
+}
+
+export default App;
