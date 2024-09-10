26352cdf74ac8986decc89205c922dbb1d8c9a63
cozytuba <cozytuba1533@gmail.com>
2024-09-10 22:14:26 +0530

    Adding more logic to textutils

--- project-1_textutils/src/6_components/TextForm.js
+++ project-1_textutils/src/6_components/TextForm.js
@@ -4,13 +4,18 @@ export default function
 (props) {
         // use of state 
         // text will  be updated using setText function
-        const [text,setText]=useState("Enter text here");
+        const [text,setText]=useState('');
         // text="sdasd";    wrong way to update text
         // setText("new");  correct way to update text state     
        
         const handleUppercase=()=>{
             console.log("uppercase clicked "+text)
             setText(text.toUpperCase()  );
+           
+        }
+        const handleLowercase=()=>{
+            console.log("uppercase clicked "+text)
+            setText(text.toLowerCase() );
            
         }
 
@@ -22,15 +27,32 @@ export default function
 
 
     return (
-        <div>
+        <>
+         <div>
             <div className="mb-3">
                 <h1>{props.heading} </h1>
 
                 <textarea className="form-control" value={text} onChange={handleOnChange} id="box" rows="9"></textarea>
             </div>
+           
             <button className="btn btn-primary" onClick={handleUppercase}>Uppercase</button>
+            <button className="btn btn-primary mx-2" onClick={handleLowercase}>Lowercase</button>
+        </div>
+        
+        <div className="container my-2">
+            <h2>
+                Text Summary
+            </h2>
+            <p>{text.split(' ').length} words and {text.length} characters</p>
+            <p>{text.split(' ').length * (0.008)} Minutes to read</p>
+            <h2>
+                Preview
+            </h2>
+            <p>{text}</p>
         </div>
 
+        </>
+       
 
     )
 }
