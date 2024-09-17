347841df11e583cef4f573b92732f2da8aa54cf1
cozytuba <cozytuba1533@gmail.com>
2024-09-17 22:31:42 +0530

    9 Exercise

--- project-1_textutils/src/6_components/TextForm.js
+++ project-1_textutils/src/6_components/TextForm.js
@@ -1,13 +1,15 @@
 import React,{useState} from 'react'
-
+// import translate from 'translate';
+// const translate=require('translate');
 export default function
 (props) {
-        // use of state 
-        // text will  be updated using setText function
-        const [text,setText]=useState('');
-        // text="sdasd";    wrong way to update text
-        // setText("new");  correct way to update text state     
-       
+    // use of state 
+    // text will  be updated using setText function
+    const [text,setText]=useState('');
+    // text="sdasd";    wrong way to update text
+    // setText("new");  correct way to update text state     
+    
+        
         const handleUppercase=()=>{
             console.log("uppercase clicked "+text)
             setText(text.toUpperCase()  );
@@ -24,7 +26,14 @@ export default function
             // console.log(event.target.value);
             setText(event.target.value);
         }
+        const Text_To_Speech=(event)=>{
+            const voice=speechSynthesis.getVoices();
+            const word=new SpeechSynthesisUtterance(text);
+            word.voice =voice[21];
+            speechSynthesis.speak(word);
+        }
 
+     
 
     return (
         <>
@@ -37,6 +46,8 @@ export default function
            
             <button className="btn btn-primary" onClick={handleUppercase}>Uppercase</button>
             <button className="btn btn-primary mx-2" onClick={handleLowercase}>Lowercase</button>
+            <button className="btn btn-primary mx-2" onClick={Text_To_Speech}>Text-to-Speech</button>
+            {/* <button className="btn btn-primary mx-2" onClick={handleLanguageChange}>Translate</button> */}
         </div>
         
         <div className="container my-2">
