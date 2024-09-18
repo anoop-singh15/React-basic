4f313cd8509ca4407463aae8daff873cedef3a34
cozytuba <cozytuba1533@gmail.com>
2024-09-18 22:00:12 +0530

    9 Enabling dark mode

--- project-1_textutils/src/App.js
+++ project-1_textutils/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import Navbar from './6_components/Navbar'
 import TextForm from './6_components/TextForm';
+import About from './6_components/About';
 
 // let a = "REACT";
 function App() {
@@ -14,6 +15,7 @@ function App() {
      {/* Changes in 7-state and event handling */}
      <div className="container">
      <TextForm heading="Text to Analyze"/>
+     <About/>
 
      </div>
 
